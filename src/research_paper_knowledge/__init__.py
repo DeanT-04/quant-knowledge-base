9ec35f0ee621f16ac3cd73f38e11ab77620d57aa
DeanT-04 <DeanT-04@users.noreply.github.com>
2026-07-23 11:51:11 +0100

    Clean up repository, keeping only pdfs folder

--- src/research_paper_knowledge/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Research Paper Knowledge Package."""
-
-from research_paper_knowledge.parser import PaperParser, parse_pdf_directory
-
-__all__ = [
-    "PaperParser",
-    "parse_pdf_directory",
-]
-
